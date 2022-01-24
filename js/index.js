dc96d9c0b3be68d5d0dd11514dd6fab3b3b64175
Oleg <kn1feR26rus@yandex.ru>
2022-01-25 00:10:51 +0300

    add like-counter

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 $(document).ready(function () {
-    const nextButton = $('#main-arrow-right');
-    const prevButton = $('#main-arrow-left');
-    const mainUrlrl = 'https://private-anon-9390dbdbde-grchhtml.apiary-mock.com/slides?offset=0&limit=3';
     let allSlides = [];
-    let countSlides = 3;
-    let missSlides = 0;
+    let likeCounter = $('<p class="footer_like-counter"></p>')
     function getNewUrl (missSlides, countNextSlides) {
         return 'https://private-anon-9390dbdbde-grchhtml.apiary-mock.com/slides?offset=' + missSlides + '&limit=' + countNextSlides;
     }
@@ -20,12 +16,19 @@ $(document).ready(function () {
       }
       
       function displayTasks(dataFromServer) {
+        
         $(dataFromServer).each(function (i) {
             let item = dataFromServer[i];
-            let slide = $('<div></div>').addClass('swiper-slide').attr('id', item.id).css('background-image', 'url(' + item.imgUrl + ')');
+            let slide = $('<div></div>').addClass('swiper-slide').attr('id', item.id).css('background-image', 'url(' + item.imgUrl + ')').attr('data-likeCnt', item.likeCnt);
             swiper.appendSlide(slide);
+            if($('.swiper-slide-active')[0].id == item.id) {
+              $('.footer_like-countContainer').append(likeCounter)
+              $(likeCounter).text(item.likeCnt);
+            }
         });
+        
     }
+
 
     let swiper = new Swiper(".mySwiper", {
       slidesPerView: 1,
@@ -40,11 +43,16 @@ $(document).ready(function () {
     
     function getSlides(response) {
       allSlides = response.data;
+      $('.swiper-button-next').click(function(){
+        $(likeCounter).text($('.swiper-slide-active').attr('data-likeCnt'));
+      })
+      $('.swiper-button-prev').click(function(){
+        $(likeCounter).text($('.swiper-slide-active').attr('data-likeCnt'));
+      })
       displayTasks(allSlides);
-      }
+    }
  
     $('.swiper-button-next').click(function(){
-      console.log(swiper);
         if (swiper.isEnd && swiper.realIndex == 2) {
           showSlides(3, 3);
         }
@@ -52,6 +60,8 @@ $(document).ready(function () {
           showSlides(6, 3);
         }
     })
+
+    
 
     function showSlides(missData, countData) {
       ajaxRequest(getNewUrl(missData, countData), 'GET', getSlides);
